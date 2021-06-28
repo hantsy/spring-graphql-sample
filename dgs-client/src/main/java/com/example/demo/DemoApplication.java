dd401ea3bc79bc97e0c9f5ed6242e269cb02802a
Hantsy Bai <hantsy@gmail.com>
2021-06-28 15:31:33 +0800

    add subscription.

--- dgs-client/src/main/java/com/example/demo/DemoApplication.java
+++ dgs-client/src/main/java/com/example/demo/DemoApplication.java
@@ -24,6 +24,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootApplication
 @Slf4j
@@ -37,6 +38,28 @@ public class DemoApplication implements ApplicationRunner {
 
     @Value("${graphql.client.url}")
     private String url;
+
+    private static HttpResponse execute(String url, Map<String, ? extends List<String>> headers, String body) {
+        /**
+         * The requestHeaders providers headers typically required to call a GraphQL endpoint, including the Accept and Content-Type headers.
+         * To use RestTemplate, the requestHeaders need to be transformed into Spring's HttpHeaders.
+         */
+        HttpHeaders requestHeaders = new HttpHeaders();
+        headers.forEach(requestHeaders::put);
+
+        /**
+         * Use RestTemplate to call the GraphQL service.
+         * The response type should simply be String, because the parsing will be done by the GraphQLClient.
+         */
+        var dgsRestTemplate = new RestTemplate();
+        ResponseEntity<String> exchange = dgsRestTemplate.exchange(url, HttpMethod.POST, new HttpEntity(body, requestHeaders), String.class);
+
+        /**
+         * Return a HttpResponse, which contains the HTTP status code and response body (as a String).
+         * The way to get these depend on the HTTP client.
+         */
+        return new HttpResponse(exchange.getStatusCodeValue(), exchange.getBody());
+    }
 
 
     @Override
@@ -78,27 +101,7 @@ public class DemoApplication implements ApplicationRunner {
 
         String query = graphQLQueryRequest.serialize();
         GraphQLClient client = new DefaultGraphQLClient(url);
-        GraphQLResponse response = client.executeQuery(query, new HashMap<>(), (url, headers, body) -> {
-            /**
-             * The requestHeaders providers headers typically required to call a GraphQL endpoint, including the Accept and Content-Type headers.
-             * To use RestTemplate, the requestHeaders need to be transformed into Spring's HttpHeaders.
-             */
-            HttpHeaders requestHeaders = new HttpHeaders();
-            headers.forEach(requestHeaders::put);
-
-            /**
-             * Use RestTemplate to call the GraphQL service.
-             * The response type should simply be String, because the parsing will be done by the GraphQLClient.
-             */
-            var dgsRestTemplate = new RestTemplate();
-            ResponseEntity<String> exchange = dgsRestTemplate.exchange(url, HttpMethod.POST, new HttpEntity(body, requestHeaders), String.class);
-
-            /**
-             * Return a HttpResponse, which contains the HTTP status code and response body (as a String).
-             * The way to get these depend on the HTTP client.
-             */
-            return new HttpResponse(exchange.getStatusCodeValue(), exchange.getBody());
-        });
+        GraphQLResponse response = client.executeQuery(query, new HashMap<>(), DemoApplication::execute);
 
         var data = response.extractValueAsObject("allPosts", new TypeRef<List<Post>>() {
         });
