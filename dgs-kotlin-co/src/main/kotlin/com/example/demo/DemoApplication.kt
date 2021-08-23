8edfe1d9500c7be3c17ca3ddb1df1a5eb14ba543
hantsy <hantsy@gmail.com>
2021-08-23 12:17:25 +0800

    chore: clean codes

--- dgs-kotlin-co/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ dgs-kotlin-co/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -12,6 +12,7 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
+import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.config.web.server.invoke
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -45,7 +46,7 @@ class DataInitializer(val posts: PostRepository, val comments: CommentRepository
 }
 
 @Configuration
-@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
+@EnableReactiveMethodSecurity
 class SecurityConfig {
 
     @Bean
