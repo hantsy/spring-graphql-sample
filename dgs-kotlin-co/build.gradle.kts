4c50c904393405d5e146a723006e9c755e4cad1a
hantsy <hantsy@gmail.com>
2021-08-31 20:51:34 +0800

    chore: bump to Java 17

--- dgs-kotlin-co/build.gradle.kts
+++ dgs-kotlin-co/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_16
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
