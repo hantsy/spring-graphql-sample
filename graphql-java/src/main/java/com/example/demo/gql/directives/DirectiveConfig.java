df1ea9c5887c5f90dc6655a417b558018f195f28
hantsy <hantsy@gmail.com>
2021-09-20 20:35:23 +0800

    chore: clean codes

--- graphql-java/src/main/java/com/example/demo/gql/directives/DirectiveConfig.java
+++ graphql-java/src/main/java/com/example/demo/gql/directives/DirectiveConfig.java
@@ -1,6 +1,6 @@
 package com.example.demo.gql.directives;
 
-import graphql.kickstart.tools.boot.SchemaDirective;
+import graphql.kickstart.autoconfigure.tools.SchemaDirective;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
