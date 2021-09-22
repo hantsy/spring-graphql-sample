69e5d08760ae1824f3931411817f6e7dfceea07c
hantsy <hantsy@gmail.com>
2021-09-22 14:41:52 +0800

    chore: clean codes

--- dgs-codegen/src/test/java/com/example/demo/MutationTests.java
+++ dgs-codegen/src/test/java/com/example/demo/MutationTests.java
@@ -2,13 +2,19 @@ package com.example.demo;
 
 import com.example.demo.gql.client.CreatePostGraphQLQuery;
 import com.example.demo.gql.types.CreatePostInput;
+import com.example.demo.service.PostService;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class MutationTests {
@@ -16,8 +22,32 @@ class MutationTests {
     @Autowired
     DgsQueryExecutor dgsQueryExecutor;
 
+    @MockBean
+    PostService postService;
+
+    @Test
+    void testCreatePost() {
+        UUID id = UUID.randomUUID();
+        when(postService.createPost(any(CreatePostInput.class))).thenReturn(id);
+        var queryRequest = new GraphQLQueryRequest(
+                CreatePostGraphQLQuery.newRequest()
+                        .createPostInput(
+                                CreatePostInput.newBuilder()
+                                        .title("test title")
+                                        .content("content of test")
+                                        .build()
+                        )
+                        .build()
+        );
+        String result = dgsQueryExecutor.executeAndExtractJsonPath(queryRequest.serialize(), "data.createPost");
+        assertThat(result).isEqualTo(id.toString());
+        verify(postService,times(1)).createPost(any(CreatePostInput.class));
+        verifyNoMoreInteractions(postService);
+    }
+
     @Test
     void testValidateCreatePost() {
+        when(postService.createPost(any(CreatePostInput.class))).thenReturn(UUID.randomUUID());
         var queryRequest = new GraphQLQueryRequest(
                 CreatePostGraphQLQuery.newRequest()
                         .createPostInput(
@@ -30,6 +60,7 @@ class MutationTests {
         );
         var result = dgsQueryExecutor.execute(queryRequest.serialize());
         assertThat(result.getErrors()).isNotEmpty();
+        verifyNoInteractions(postService);
     }
 
 }
--- dgs-codegen/src/test/java/com/example/demo/QueryTests.java
+++ dgs-codegen/src/test/java/com/example/demo/QueryTests.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import com.example.demo.gql.client.AllPostsGraphQLQuery;
 import com.example.demo.gql.client.AllPostsProjectionRoot;
 import com.example.demo.gql.types.Post;
+import com.example.demo.service.PostNotFoundException;
 import com.example.demo.service.PostService;
 import com.netflix.graphql.dgs.DgsQueryExecutor;
 import com.netflix.graphql.dgs.client.codegen.GraphQLQueryRequest;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -75,6 +78,41 @@ class QueryTests {
 
         assertThat(titles).anyMatch(s -> s.startsWith("DGS POST"));
         verify(postService, times(1)).getAllPosts();
+        verifyNoMoreInteractions(postService);
+    }
+
+    @Test
+    void postById() {
+        when(postService.getPostById(anyString())).thenReturn(
+                Post.newBuilder().id(UUID.randomUUID().toString())
+                        .title("test title")
+                        .content("test content")
+                        .build()
+        );
+        String title = dgsQueryExecutor.executeAndExtractJsonPath(
+                "query postById($postId:String!){ postById(postId:$postId) { title content }}",
+                "data.postById.title",
+                Map.of("postId", "test")
+        );
+
+        assertThat(title).isEqualTo("TEST TITLE");
+
+        verify(postService, times(1)).getPostById(anyString());
+        verifyNoMoreInteractions(postService);
+    }
+
+    @Test
+    void postById_notFound() {
+        when(postService.getPostById(anyString())).thenThrow(new PostNotFoundException("test"));
+        var executeResult = dgsQueryExecutor.execute(
+                "query postById($postId:String!){ postById(postId:$postId) { title content }}",
+                Map.of("postId", "test")
+        );
+
+        assertThat(executeResult.getErrors()).isNotEmpty();
+        assertThat(executeResult.getErrors().get(0).getMessage()).isEqualTo("Post: test was not found.");
+
+        verify(postService, times(1)).getPostById(anyString());
         verifyNoMoreInteractions(postService);
     }
 }
