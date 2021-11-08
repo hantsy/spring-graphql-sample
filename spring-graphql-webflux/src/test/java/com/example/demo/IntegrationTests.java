13e3f290944224c5581447fef27b7a1c4fdbe8cc
hantsy <hantsy@gmail.com>
2021-11-08 20:17:00 +0800

    chore(dep): bump to dgs 4.9.3

--- spring-graphql-webflux/src/test/java/com/example/demo/IntegrationTests.java
+++ spring-graphql-webflux/src/test/java/com/example/demo/IntegrationTests.java
@@ -19,6 +19,7 @@ import java.net.URI;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
