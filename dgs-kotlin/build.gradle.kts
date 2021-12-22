ce9de7f0dc8144e9f7f38b703cc52a5228da0514
hantsy <hantsy@gmail.com>
2021-12-22 21:35:24 +0800

    chore: bump to Spring Boot 2.6.2.

--- dgs-kotlin/build.gradle.kts
+++ dgs-kotlin/build.gradle.kts
@@ -2,7 +2,7 @@ import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.1"
+    id("org.springframework.boot") version "2.6.2"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
