5e237933354450acad7ae81bea6fad5b3a34c9fa
hantsy <hantsy@gmail.com>
2021-12-27 20:15:46 +0800

    chore: bump to Dgs 4.9.15

--- dgs-kotlin/build.gradle.kts
+++ dgs-kotlin/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 dependencies {
     //implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:4.7.6-SNAPSHOT"))
 
-    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:4.9.11"))
+    implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:4.9.15"))
     implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter") {
         exclude("org.yaml", "snakeyaml")
     }
