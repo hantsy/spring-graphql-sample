f57add0f056d4170cf9d6e88fcf786c90a39b2ad
Hantsy Bai <hantsy@gmail.com>
2022-01-10 20:47:53 +0800

    Update build.gradle.kts

--- dgs-kotlin-co/build.gradle.kts
+++ dgs-kotlin-co/build.gradle.kts
@@ -61,7 +61,7 @@ tasks.withType<GenerateJavaTask> {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
