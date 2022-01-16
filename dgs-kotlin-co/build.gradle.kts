a0e2b77c8eba7832960ef64d020442956323f0fa
hantsy <hantsy@gmail.com>
2022-01-16 15:24:34 +0800

    chore: clean kotlin-co example

--- dgs-kotlin-co/build.gradle.kts
+++ dgs-kotlin-co/build.gradle.kts
@@ -63,12 +63,11 @@ tasks.withType<GenerateJavaTask> {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
+        freeCompilerArgs = listOf("-Xjsr305=strict", "-opt-in=kotlin.RequiresOptIn")
         jvmTarget = "17"
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
-
 }
