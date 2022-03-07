85787411d3553a63ca25650597823bbbb5179047
hantsy <hantsy@gmail.com>
2022-03-07 22:16:41 +0800

    chore: bump to dgs 4.9.21

--- dgs-subscription-ws/build.gradle.kts
+++ dgs-subscription-ws/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 	kotlin("jvm") version "1.6.10"
 	kotlin("plugin.spring") version "1.6.10"
 
-	id("com.netflix.dgs.codegen") version "5.1.16" //https://plugins.gradle.org/plugin/com.netflix.dgs.codegen
+	id("com.netflix.dgs.codegen") version "5.1.17" //https://plugins.gradle.org/plugin/com.netflix.dgs.codegen
 }
 
 group = "com.example"
@@ -20,7 +20,7 @@ repositories {
 }
 
 dependencies {
-	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:4.9.20"))
+	implementation(platform("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:4.9.21"))
 	implementation("com.netflix.graphql.dgs:graphql-dgs-spring-boot-starter") {
 		exclude("org.yaml", "snakeyaml")
 	}
