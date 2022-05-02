d7e8ffb7cfd0e213ca2739d478cfac2d1001ea85
hantsy <hantsy@gmail.com>
2022-05-02 14:26:15 +0800

    chore: Bump to Spring Boot 2.6.7

--- dgs-subscription-sse/build.gradle.kts
+++ dgs-subscription-sse/build.gradle.kts
@@ -2,10 +2,10 @@ import com.netflix.graphql.dgs.codegen.gradle.GenerateJavaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.6.6"
+	id("org.springframework.boot") version "2.6.7"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.6.20"
-	kotlin("plugin.spring") version "1.6.20"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
 	id("com.netflix.dgs.codegen") version "5.1.17" //https://plugins.gradle.org/plugin/com.netflix.dgs.codegen
 }
 
