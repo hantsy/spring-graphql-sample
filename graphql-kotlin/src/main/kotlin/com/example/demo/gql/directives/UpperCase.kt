c794b4259f2844a07425d0b2ada0131f2e47c88f
hantsy <hantsy@gmail.com>
2022-08-28 19:41:52 +0800

    chore: clean codes

--- graphql-kotlin/src/main/kotlin/com/example/demo/gql/directives/UpperCase.kt
+++ graphql-kotlin/src/main/kotlin/com/example/demo/gql/directives/UpperCase.kt
@@ -1,7 +1,7 @@
 package com.example.demo.gql.directives
 
 import com.expediagroup.graphql.generator.annotations.GraphQLDirective
-import graphql.introspection.Introspection
+import graphql.introspection.Introspection.DirectiveLocation.FIELD_DEFINITION
 
-@GraphQLDirective(name = UpperCaseDirectiveWiring.name, locations = arrayOf(Introspection.DirectiveLocation.FIELD_DEFINITION))
-annotation class UpperCase()
+@GraphQLDirective(name = UpperCaseDirectiveWiring.name, locations = [FIELD_DEFINITION])
+annotation class UpperCase
