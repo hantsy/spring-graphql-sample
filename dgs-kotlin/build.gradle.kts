a229bbd86adf270efb897ebc7bdf12cee5fe859d
hantsy <hantsy@gmail.com>
2022-10-25 22:55:30 +0800

    chore: bump dependency manager plugin to 1.1.0

--- dgs-kotlin/build.gradle.kts
+++ dgs-kotlin/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.springframework.boot") version "2.7.5"
-    id("io.spring.dependency-management") version "1.0.14.RELEASE"
+    id("io.spring.dependency-management") version "1.1.0"
     kotlin("jvm") version "1.7.20"
     kotlin("plugin.spring") version "1.7.20"
     id("com.netflix.dgs.codegen") version "5.4.0" //https://plugins.gradle.org/plugin/com.netflix.dgs.codegen
