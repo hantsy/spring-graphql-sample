b968846aadc592f3828d988669c44245bb30e926
hantsy <hantsy@gmail.com>
2022-12-06 20:16:57 +0800

    chore: clean codes

--- spring-graphql-querydsl/src/main/java/com/example/demo/model/Comment.java
+++ spring-graphql-querydsl/src/main/java/com/example/demo/model/Comment.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
