3d604f1fe05278397d4ae110a5e430204defadb8
hantsy <hantsy@gmail.com>
2023-01-21 13:51:02 +0800

    chore: Bump Kotlin to 1.8.0

--- spring-graphql-rsocket-kotlin-co/build.gradle.kts
+++ spring-graphql-rsocket-kotlin-co/build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.springframework.boot") version "3.0.0"
 	id("io.spring.dependency-management") version "1.1.0"
 
-	kotlin("jvm") version "1.7.22"
-	kotlin("plugin.spring") version "1.7.22"
+	kotlin("jvm") version "1.8.0"
+	kotlin("plugin.spring") version "1.8.0"
 
 }
 
