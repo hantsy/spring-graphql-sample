61ccd369aae005b91923d24814873b28d1184bdf
hantsy <hantsy@gmail.com>
2023-05-27 14:50:26 +0800

    chore: clean codes

--- spring-graphql-rsocket-kotlin-co/build.gradle.kts
+++ spring-graphql-rsocket-kotlin-co/build.gradle.kts
@@ -1,12 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.0.6"
+	id("org.springframework.boot") version "3.1.0"
 	id("io.spring.dependency-management") version "1.1.0"
 
 	kotlin("jvm") version "1.8.21"
 	kotlin("plugin.spring") version "1.8.21"
-
 }
 
 group = "com.example"
@@ -16,6 +15,11 @@ java.sourceCompatibility = JavaVersion.VERSION_17
 repositories {
 	mavenCentral()
 }
+
+extra["coroutinesVersion"]="1.7.1"
+extra["mockkVersion"]="1.13.5"
+extra["springmockkVersion"]="4.0.2"
+extra["kotestVersion"]="5.6.2"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-graphql")
@@ -32,24 +36,27 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
 	//kotlin coroutines
-	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:1.7.1")
-	
+	developmentOnly("org.jetbrains.kotlinx:kotlinx-coroutines-debug:${property("coroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:${property("coroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:${property("coroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:${property("coroutinesVersion")}")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${property("coroutinesVersion")}")
+
 	//test
 	testImplementation("org.springframework.graphql:spring-graphql-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(module = "mockito-core")
 	}
 	testImplementation("io.projectreactor:reactor-test")
-	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
-	testImplementation("io.mockk:mockk-jvm:1.13.5")
-	testImplementation("com.ninja-squad:springmockk:4.0.2"){
+	testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test-jvm:${property("coroutinesVersion")}")
+	testImplementation("io.mockk:mockk-jvm:${property("mockkVersion")}")
+	testImplementation("com.ninja-squad:springmockk:${property("springmockkVersion")}"){
 		exclude(module = "mockk")
 	}
 
-	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.6.2")
-	testImplementation("io.kotest:kotest-assertions-core-jvm:5.6.2")
-	testImplementation("io.kotest:kotest-framework-concurrency:5.6.2")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:${property("kotestVersion")}")
+	testImplementation("io.kotest:kotest-assertions-core-jvm:${property("kotestVersion")}")
+	testImplementation("io.kotest:kotest-framework-concurrency:${property("kotestVersion")}")
 }
 
 tasks.withType<KotlinCompile> {
