016d60b0599dbc762a4ef815887217a781561f68
hantsy <hantsy@gmail.com>
2024-04-16 08:57:45 +0800

    chore: update deps

--- dgs-subscription-sse/build.gradle.kts
+++ dgs-subscription-sse/build.gradle.kts
@@ -24,7 +24,7 @@ repositories {
 
 dependencyManagement {
     imports {
-        mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.5.0")
+        mavenBom("com.netflix.graphql.dgs:graphql-dgs-platform-dependencies:8.5.5")
     }
 }
 
